3c022fa185d3931b351f4fcf96f66db70d0116ed
Yechiel Kalmenson <contact@yechiel.me>
2018-01-11 21:59:48 -0500

    add validation to make sure the form has data

--- elvis_store/sendemail.php
+++ elvis_store/sendemail.php
@@ -4,29 +4,31 @@
     $msg = $_POST['elvismail'];
     $from = 'elvisstore@yechiel.me';
 
-    $dbc = mysqli_connect('localhost', 'yechielk', 'testtest', 'elvis_store')
-        or die('Error connecting to DB');
+    if (!empty($subject) && !empty($msg)) {
 
-    $query = "SELECT * FROM email_list";
-    
-    $result = mysqli_query($dbc, $query)
-        or die('error inserting into database');
+        $dbc = mysqli_connect('localhost', 'yechielk', 'testtest', 'elvis_store')
+            or die('Error connecting to DB');
 
-    while($row = mysqli_fetch_array($result)){
+        $query = "SELECT * FROM email_list";
+        
+        $result = mysqli_query($dbc, $query)
+            or die('error inserting into database');
 
-        $first_name = $row['first_name'];
-        $last_name = $row['last_name'];
-        $to = $row['email'];
+        while($row = mysqli_fetch_array($result)){
 
-        if(mail($to, $subject, $msg, 'From:' . $from)){
-            echo "Emailed " . $first_name . " " . $last_name . "<br>";
+            $first_name = $row['first_name'];
+            $last_name = $row['last_name'];
+            $to = $row['email'];
 
-        } else {
-            echo "failed to email " . $first_name . " " . $last_name . "<br>";
-        }
+            if(mail($to, $subject, $msg, 'From:' . $from)){
+                echo "Emailed " . $first_name . " " . $last_name . "<br>";
 
-    };
+            } else {
+                echo "failed to email " . $first_name . " " . $last_name . "<br>";
+            }
 
-    mysqli_close($dbc);
+        };
 
+        mysqli_close($dbc);
+    }
 ?>
