e704b1110c2963ab124bc6d2a513b968e0b7e365
Yechiel Kalmenson <contact@yechiel.me>
2018-01-11 23:30:59 -0500

    make a checkbox form for the removeemail script

--- elvis_store/removeemail.php
+++ elvis_store/removeemail.php
@@ -1,14 +1,23 @@
-<?php
-  $email = $_POST['email'];
+<img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+<img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+<p>Enter an email address to remove.</p>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+  <?php
+      $dbc = mysqli_connect('localhost', 'yechielk', 'testtest', 'elvis_store')
+        or die('Error connecting to DB');
 
-  $dbc = mysqli_connect('localhost', 'yechielk', 'testtest', 'elvis_store')
-  or die('Error connecting to DB');
+      $query = "SELECT * FROM email_list";
 
-  $query = "DELETE FROM email_list WHERE email = '$email'";
+      $result = mysqli_query($dbc, $query);
 
-  mysqli_query($dbc, $query)
-    or die('error deleting from database');
+      while($row = mysqli_fetch_array($result)){
+        echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]" />';
+        echo $row['first_name'] . " " . $row['last_name'] . " - " . $row['email'] . '<br>';
+      }
 
-  mysqli_close($dbc);
+      mysqli_close($dbc);
+  ?>
+  <input type="submit" name="submit" value="Remove" />
+</form>
 
-?>
+
