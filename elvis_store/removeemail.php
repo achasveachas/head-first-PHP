9612f92f9285748fffa1ae9d24ffe32c5b34c7a3
Yechiel Kalmenson <contact@yechiel.me>
2018-01-13 21:50:17 -0500

    add delete action

--- elvis_store/removeemail.php
+++ elvis_store/removeemail.php
@@ -1,10 +1,24 @@
 <img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
 <img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+
+<?php
+  $dbc = mysqli_connect('localhost', 'yechielk', 'testtest', 'elvis_store')
+    or die('Error connecting to DB');
+
+  if(isset($_POST['submit'])) {
+    foreach ($_POST['todelete'] as $id_to_delete) {
+      $query = "DELETE FROM email_list WHERE id = $id_to_delete";
+      mysqli_query($dbc, $query)
+        or die("Error deleting from DB");
+
+    }
+    echo "Customers deleted.<br>";
+  } 
+?>
 <p>Enter an email address to remove.</p>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
   <?php
-      $dbc = mysqli_connect('localhost', 'yechielk', 'testtest', 'elvis_store')
-        or die('Error connecting to DB');
+
 
       $query = "SELECT * FROM email_list";
 
